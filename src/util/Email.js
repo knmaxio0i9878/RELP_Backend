7e88217b6792338669b7c35e306e12aeeaec60a0
knmaxio0i9878 <pmakwana1908@gmail.com>
2024-06-19 19:40:58 +0530

    Emailchgange

--- src/util/Email.js
+++ src/util/Email.js
@@ -10,5 +10,18 @@ const sendingMail = async(to,subject,text) =>{
             pass:'ygpiwimazhbdotty'
         }
     })
+
+    const mailOptions = {
+        from: "pmakwana1908@gmail.com",
+        to: to,
+        subject: subject,
+        html:`<h1>${text}</h1>`
+      };
+    
+
+    const response = await transporter.sendMail(mailOptions);
+    return response;
+
+
 }
-module.exports={sendingMail}
+module.exports={sendingMail,}
